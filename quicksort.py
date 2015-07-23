a5aebdbd95c83ee15d46fc53222bb17927ff54bc
acyso <adrian.c.so@gmail.com>
2015-07-22 23:16:05 -0500

    add test cases, working version of the left pivot

--- quicksort.py
+++ quicksort.py
@@ -1,31 +1,26 @@
-import unittest
-
-def quicksort(A):
-    print("called quicksort with " + str(A))
+def quicksort(A, i, j):
     if len(A) <= 1:
         return
-    pivot_index = get_pivot_index()
-    partition(A, pivot_index)
+    correct_pivot_index = partition(A, i, j)
 
-    #here's the error: where does the pivot_index_value end up?
-    quicksort(A[:pivot_index])
-    quicksort(A[pivot_index+1:])
+    N = j - i - 1
 
+    if correct_pivot_index - i >= 1:
+        N += quicksort(A, i, correct_pivot_index)
+    if j - correct_pivot_index - 1 >= 1:
+        N += quicksort(A, correct_pivot_index + 1, j)
 
-def get_pivot_index():
-    return 0
+    return N
 
-
-def partition(A, index):
-    if len(A) <= 1:
-        return
-    pivot = A[index]
-    i = index + 1
-    for j in range(i, len(A)):
+def partition(A, left, right):
+    pivot = A[left] #modify this!
+    i = left + 1
+    for j in range(left+1, right):
         if A[j] < pivot:
             swap(A, i, j)
             i += 1
-    swap(A, index, i - 1)
+    swap(A, left, i - 1)
+    return i - 1
 
 
 def swap(A, i, j):
@@ -34,20 +29,7 @@ def swap(A, i, j):
     A[j] = tmp
 
 
-class Tests(unittest.TestCase):
-
-    def test_swap(self):
-        A = [1, 2, 3, 4, 5]
-        swap(A, 1, 2)
-        self.assertSequenceEqual([1, 3, 2, 4, 5], A)
-
-    def test_partition(self):
-        A = [3, 8, 2, 5, 1, 4, 7, 6]
-        partition(A, 0)
-        self.assertSequenceEqual([1, 2, 3, 5, 8, 4, 7, 6], A)
-
 if __name__ == '__main__':
-    #unittest.main()
     A = [3, 8, 2, 5, 1, 4, 7, 6]
-    quicksort(A)
+    print(quicksort(A, 0, 8))
     print(A)
