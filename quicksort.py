bc60546e596b1c6079ad9aabba2d27eb1905c62f
acyso <adrian.c.so@gmail.com>
2015-07-24 00:10:05 -0500

    add right pivot

--- quicksort.py
+++ quicksort.py
@@ -1,18 +1,22 @@
-def quicksort(A, i, j):
+def quicksort(A, i, j, method):
     if len(A) <= 1:
         return
-    correct_pivot_index = partition(A, i, j)
+
+    if method == 'left':
+        correct_pivot_index = partition_left(A, i, j)
+    elif method == 'right':
+        correct_pivot_index = partition_right(A, i, j)
 
     N = j - i - 1
 
     if correct_pivot_index - i >= 1:
-        N += quicksort(A, i, correct_pivot_index)
+        N += quicksort(A, i, correct_pivot_index, method)
     if j - correct_pivot_index - 1 >= 1:
-        N += quicksort(A, correct_pivot_index + 1, j)
+        N += quicksort(A, correct_pivot_index + 1, j, method)
 
     return N
 
-def partition(A, left, right):
+def partition_left(A, left, right):
     pivot = A[left] #modify this!
     i = left + 1
     for j in range(left+1, right):
@@ -23,6 +27,17 @@ def partition(A, left, right):
     return i - 1
 
 
+def partition_right(A, left, right):
+    swap(A, right-1, left)
+    pivot = A[left]
+    i = left + 1
+    for j in range(left+1, right):
+        if A[j] < pivot:
+            swap(A, i, j)
+            i += 1
+    swap(A, left, i - 1)
+    return i - 1
+
 def swap(A, i, j):
     tmp = A[i]
     A[i] = A[j]
@@ -31,5 +46,5 @@ def swap(A, i, j):
 
 if __name__ == '__main__':
     A = [3, 8, 2, 5, 1, 4, 7, 6]
-    print(quicksort(A, 0, 8))
+    print(quicksort(A, 0, 8, 'left'))
     print(A)
--- test_quicksort.py
+++ test_quicksort.py
@@ -29,27 +29,46 @@ class Tests(unittest.TestCase):
         swap(A, 1, 2)
         self.assertSequenceEqual([1, 3, 2, 4, 5], A)
 
-    def test_partition(self):
+    def test_partition_left(self):
         A = [3, 8, 2, 5, 1, 4, 7, 6]
-        partition(A, 0, 8)
+        partition_left(A, 0, 8)
         self.assertSequenceEqual([1, 2, 3, 5, 8, 4, 7, 6], A)
 
     def test_left_10(self):
         A = load10()
-        q = quicksort(A, 0, 10)
+        q = quicksort(A, 0, 10, 'left')
         self.assertEqual(25, q)
+        self.assertSequenceEqual(range(1, 11), A)
+
+    def test_right_10(self):
+        A = load10()
+        q = quicksort(A, 0, 10, 'right')
+        self.assertEqual(29, q)
         self.assertSequenceEqual(range(1, 11), A)
 
     def test_left_100(self):
         A = load100()
-        q = quicksort(A, 0, 100)
+        q = quicksort(A, 0, 100, 'left')
         self.assertEqual(615, q)
+        self.assertSequenceEqual(range(1, 101), A)
+
+    def test_right_100(self):
+        A = load100()
+        q = quicksort(A, 0, 100, 'right')
+        self.assertEqual(587, q)
         self.assertSequenceEqual(range(1, 101), A)
 
     def test_left_1000(self):
         A = load1000()
-        q = quicksort(A, 0, 1000)
+        q = quicksort(A, 0, 1000, 'left')
         self.assertEqual(10297, q)
+        self.assertSequenceEqual(range(1, 1001), A)
+
+
+    def test_right_1000(self):
+        A = load1000()
+        q = quicksort(A, 0, 1000, 'right')
+        self.assertEqual(10184, q)
         self.assertSequenceEqual(range(1, 1001), A)
 #Answers are:
 #size   first      last      median
