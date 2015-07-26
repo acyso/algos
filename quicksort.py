eb27e712c2f58cd36e0903eaa78e0f90f6433edc
acyso <adrian.c.so@gmail.com>
2015-07-26 02:03:20 -0500

    quick sort assignment all done!

--- quicksort.py
+++ quicksort.py
@@ -48,7 +48,17 @@ def partition_right(A, left, right):
 
 
 def partition_median(A, left, right):
-    pivot = get_median(A)
+    #print("calling with " + str(A) + " " + str(left) + " " + str(right))
+    pivot_index, pivot_value = get_median(A, left, right)
+    swap(A, pivot_index, left)
+    pivot = A[left]
+    i = left + 1
+    for j in range(left+1, right):
+        if A[j] < pivot:
+            swap(A, i, j)
+            i += 1
+    swap(A, left, i - 1)
+    return i - 1
 
 
 def swap(A, i, j):
@@ -57,20 +67,37 @@ def swap(A, i, j):
     A[j] = tmp
 
 
-def get_median(A):
-    middle = A[floor((len(A) - 1)/2)]
-    first = A[0]
-    last = A[-1]
+def get_median(A, left, right):
+    midindex = floor((right+left-1)/2)
+    middle = A[midindex]
+    first = A[left]
+    last = A[right-1]
     out = median([first, middle, last])
     if out == first:
-        return 0, out
+        return left, out
     elif out == last:
-        return len(A) - 1, out
+        return right-1, out
     else:
-        return floor((len(A) - 1)/2), out
+        return midindex, out
 
 
 if __name__ == '__main__':
     A = [3, 8, 2, 5, 1, 4, 7, 6]
     print(quicksort(A, 0, 8, 'left'))
     print(A)
+
+    f = open('data\QuickSort.txt')
+    B = []
+    C = []
+    D = []
+    for i in f:
+        B.append(int(i))
+        C.append(int(i))
+        D.append(int(i))
+
+    left = quicksort(B, 0, len(B), 'left')
+    print(str(left))
+    right = quicksort(C, 0, len(C), 'right')
+    print(str(right))
+    med = quicksort(D, 0, len(D), 'median')
+    print(str(med))
