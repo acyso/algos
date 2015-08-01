d81298a64fbcc32c079537279a0494dc5dcc34d4
acyso-desktop <adrian.c.so@gmail.com>
2015-08-01 14:24:01 -0500

    some small refactoring...

--- mincut.py
+++ mincut.py
@@ -6,33 +6,47 @@ def mincut(mylist):
 
 
 def fuse(mylist, edge):
-    pass
+    persisting_node = edge[0]
+    disappearing_node = edge[1]
+    mylist[persisting_node] += mylist[disappearing_node]
+    mylist[disappearing_node] = []
+
+    new_connections = []
+    for i in mylist[persisting_node]:
+        if i != persisting_node:
+            new_connections.append(i)
+
+    mylist[persisting_node] = new_connections
+
+    print(mylist)
 
 
-def select_random(mylist):
+def get_edges(d):
     ordered_pairs = []
-    for i in range(len(mylist)):
-        for j in mylist[i]:
+    for i in d:
+        for j in d[i]:
             ordered_pairs.append((i, j))
+    return ordered_pairs
 
-    rand_index = randint(0, len(ordered_pairs) -1)
+    
+def select_random_edge(edges):
+    rand_index = randint(0, len(edges) - 1
+    return edges[rand_index]
 
-    return ordered_pairs[rand_index
-]
 
-def get_number_of_edges(mylist):
+def get_number_of_edges(d):
     a = 0
-    for i in mylist:
-        a += len(i)
+    for i in d:
+        a += len(d[i])
     return a
 
 
 if __name__ == "__main__":
     f = open('data\KargerMinCut.txt')
-    B = []
+    B = {}
     for line in f:
         split = line.split()
-        B.append([int(x)-1 for x in split[1:-1]])
+        B[int(split[0])] = [int(x) for x in split[1:-1]]
 
-    for k in range(len(B)):
+    for k in range(1, len(B) + 1):
         print(str(k) + " " + str(B[k]))
--- tests/test_mincut.py
+++ tests/test_mincut.py
@@ -1,9 +1,10 @@
 import unittest
 from mincut import *
 
+
 class Tests(unittest.TestCase):
 
-    def test_swap(self):
-        A = [[1, 2], [1, 3, 4], [1]]
+    def test_get_number_of_edges(self):
+        A = {1: [1, 2], 2: [1, 3, 4], 3: [1]}
         out = get_number_of_edges(A)
         self.assertEqual(6, out)
